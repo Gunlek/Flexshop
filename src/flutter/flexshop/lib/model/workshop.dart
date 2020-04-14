29cb3eda4e67cf330a739d6e5a88ca983d35c4a6
Skeemet <Skeemet@gmail.com>
2020-04-14 15:13:11 +0200

    add conversions

--- src/flutter/flexshop/lib/model/workshop.dart
+++ src/flutter/flexshop/lib/model/workshop.dart
@@ -1,11 +1,34 @@
 class Workshop {
 
-  double id;
+  int id;
   String title;
   String image;
 
   Workshop({this.id, this.title, this.image});
 
+  Workshop.fromMapObject(Map<String, dynamic> map){
+    this.id = int.parse(map["workshop_id"].toString());
+    this.title = map["workshop_title"];
+    this.image = map["workshop_image"];
+  }
+
+  Map<String, dynamic> toMap() {
+
+    var map = Map<String, dynamic>();
+    if (this.id != null) {
+      map["workshop_id"] = this.id;
+    }
+    map["workshop_title"] = this.title;
+    map["workshop_image"] = this.image;
+    return map;
+  }
+
+  Map<String, dynamic> toJson() =>
+      {
+        'workshop_id': this.id.toString(),
+        'workshop_title': this.title,
+        'workshop_image': this.image,
+      };
 }
 
 final workshops = [
