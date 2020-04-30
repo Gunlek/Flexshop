fa3f34698a8d0cba35ee66ccdd536fd6aa2aad9b
Fabien Aubret <fabien.aubret@gmail.com>
2020-04-30 18:02:45 +0200

    [Add] Add workshop manager

---
+++ src/node/statics/js/workshops_interface.js
@@ -0,0 +1,69 @@
+let app = new Vue({
+    el: '#workshops',
+    delimiters: ['${', '}'],
+    data: {
+        workshop_list: [],
+        new_workshop_name: null,
+        new_workshop_image: null
+    },
+    methods: {
+        get_workshop_list: function(){
+            let super_this = this;
+            let url = 'http://localhost/workshops/list';
+            let httpRequest = new XMLHttpRequest();
+            httpRequest.onreadystatechange = function(data){
+                if(httpRequest.readyState === 4){
+                    if(httpRequest.status >= 200 && httpRequest.status <= 300){
+                        super_this.workshop_list = JSON.parse(httpRequest.responseText);
+                    }
+                }
+            };
+            httpRequest.open('GET', url);
+            httpRequest.send();
+        },
+        add_new_workshop: function(){
+            if(this.new_workshop_name != null && this.new_workshop_image != null){
+                let super_this = this;
+                let url = 'http://localhost/workshops/add';
+                let params = "workshop_title="+this.new_workshop_name.toString()+"&workshop_image="+this.new_workshop_image.toString();
+
+                let httpRequest = new XMLHttpRequest();
+                httpRequest.onreadystatechange = function(data){
+                    if(httpRequest.readyState === 4){
+                        if(httpRequest.status >= 200 && httpRequest.status <= 300){
+                            super_this.get_workshop_list();
+                        }
+                    }
+                };
+                httpRequest.open('POST', url);
+                httpRequest.setRequestHeader(
+                    "Content-Type",
+                    "application/x-www-form-urlencoded",
+                );
+                httpRequest.send(params);
+            }
+        },
+        del_workshop: function(id){let super_this = this;
+            let url = 'http://localhost/workshops/delete/'+id.toString();
+
+            let httpRequest = new XMLHttpRequest();
+            httpRequest.onreadystatechange = function(data){
+                if(httpRequest.readyState === 4){
+                    if(httpRequest.status >= 200 && httpRequest.status <= 300){
+                        for(let k=0; k < super_this.workshop_list.length; k++){
+                            if(super_this.workshop_list[k].workshop_id == id){
+                                super_this.workshop_list = super_this.workshop_list.splice(k, 1);
+                            }
+                        }
+                        super_this.get_workshop_list();
+                    }
+                }
+            };
+            httpRequest.open('DELETE', url);
+            httpRequest.send();
+        }
+    },
+    mounted: function(){
+        this.get_workshop_list();
+    }
+})
--- src/node/web_interface/main.js
+++ src/node/web_interface/main.js
@@ -5,8 +5,24 @@ let urlencoded = bodyParser.urlencoded({extended: true});
 let database = require('../database');
 let db = new database().getInstance();
 
-router.get('/', (req, res) => {
-    res.render('home.html.twig');
+let Workshop = require('./models/workshop.js');
+
+router.get('/', (req, res) => {res.redirect('/workshops')});
+
+router.get('/workshops', (req, res) => {
+    res.render('workshops.html.twig');
+});
+
+router.get('/categories', (req, res) => {
+    res.render('categories.html.twig');
+});
+
+router.get('/machines', (req, res) => {
+    res.render('machines.html.twig');
+});
+
+router.get('/credits', (req, res) => {
+    res.render('credits.html.twig');
 });
 
 module.exports = router;
