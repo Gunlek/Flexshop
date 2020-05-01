a31c56d591cbe1cb0b04990ee6659e7fe9f2087f
Fabien Aubret <fabien.aubret@gmail.com>
2020-05-01 10:22:00 +0200

    [Add] Add import/export capabilities

--- src/node/database.js
+++ src/node/database.js
@@ -444,6 +444,77 @@ class Database {
     }
 
     /**
+     * Créé une nouvelle entrée workshop dans la base
+     * de données en fonction du json fourni
+     * @param {json} workshop_data Tableau json représentant le workshop à créer
+     * @param {function} callback La fonction callback à appeler quand la requête a abouti
+     */
+    async createNewWorkshop(workshop_data, callback=()=>{}){
+        let db = await this.dbPromise;
+        let data_arr = [
+            workshop_data.workshop_title,
+            workshop_data.workshop_image
+        ];
+        Promise.all([
+            db.run('INSERT INTO workshops(workshop_title, workshop_image) VALUES(?, ?)', data_arr)
+        ]).then(() => callback());
+    }
+
+    /**
+     * Créé une nouvelle entrée category dans la base
+     * de données en fonction du json fourni
+     * @param {json} category_data Tableau json représentant la categorie à créer
+     * @param {function} callback La fonction callback à appeler quand la requête a abouti
+     */
+    async createNewCategory(category_data, callback=()=>{}){
+        let db = await this.dbPromise;
+        let data_arr = [
+            category_data.category_title,
+            category_data.category_workshop
+        ];
+        Promise.all([
+            db.run('INSERT INTO category(category_title, category_workshop) VALUES(?, ?)', data_arr)
+        ]).then(() => callback());
+    }
+
+    /**
+     * Créé une nouvelle entrée machine dans la base
+     * de données en fonction du json fourni
+     * @param {json} machine_data Tableau json représentant la machine à créer
+     * @param {function} callback La fonction callback à appeler quand la requête a abouti
+     */
+    async createNewMachine(machine_data, callback=()=>{}){
+        let db = await this.dbPromise;
+        let data_arr = [
+            machine_data.machine_title,
+            machine_data.machine_category,
+            machine_data.machine_brand,
+            machine_data.machine_image,
+            machine_data.machine_reference
+        ];
+        Promise.all([
+            db.run('INSERT INTO machines(machine_title, machine_category, machine_brand, machine_image, machine_reference) VALUES(?, ?, ?, ?, ?)', data_arr)
+        ]).then(() => callback());
+    }
+
+    /**
+     * Créé une nouvelle entrée section dans la base
+     * de données en fonction du json fourni
+     * @param {json} machine_data Tableau json représentant la section à créer
+     * @param {function} callback La fonction callback à appeler quand la requête a abouti
+     */
+    async createNewSection(section_data, callback=()=>{}){
+        let db = await this.dbPromise;
+        let data_arr = [
+            section_data.section_machine,
+            section_data.section_type
+        ];
+        Promise.all([
+            db.run('INSERT INTO sections(section_machine, section_type) VALUES(?, ?)', data_arr)
+        ]).then(() => callback());
+    }
+
+    /**
      * Migre les bases de données
      * /!\ Migration forcée => Effectue la migration
      * en effaçant les données !
--- src/node/web_interface/main.js
+++ src/node/web_interface/main.js
@@ -5,6 +5,12 @@ let urlencoded = bodyParser.urlencoded({extended: true});
 let database = require('../database');
 let db = new database().getInstance();
 
+let multer  = require('multer')
+let fs = require('fs');
+if(!fs.existsSync('statics/upload'))
+    fs.mkdirSync('statics/upload');
+let upload = multer({dest: 'statics/upload' });
+
 router.get('/', (req, res) => {res.redirect('/workshops')});
 
 router.get('/workshops', (req, res) => {
@@ -29,6 +35,100 @@ router.get('/credits', (req, res) => {
 
 router.get('/parameters', (req, res) => {
     res.render('parameters.html.twig');
+});
+
+router.get('/settings', (req, res) => {
+    res.render('settings.html.twig');
+})
+
+router.get('/export-json', (req, res) => {
+    db.getAllWorkshops((workshops) => {
+        db.getAllCategories((categories) => {
+            db.getAllMachines((machines) => {
+                db.getAllSections((sections) => {
+                    db.getAllParameters((parameters) => {
+                        let json_export = {}
+                        json_export['workshops'] = workshops;
+                        json_export['categories'] = categories;
+                        json_export['machines'] = machines;
+                        json_export['sections'] = sections;
+                        json_export['parameters'] = parameters;
+
+                        if(!fs.existsSync('./statics/exports')){
+                            fs.mkdirSync('./statics/exports');
+                        }
+                        fs.writeFileSync('./statics/exports/database_export.json', JSON.stringify(json_export));
+                        res.download('./statics/exports/database_export.json');
+                    });
+                });
+            });
+        });
+    });
+});
+
+router.post('/import-json', upload.single('import_file'), async (req, res) => {
+    let uploaded = req.file.path;
+    let file_content = fs.readFileSync(uploaded, {encoding: 'utf-8'});
+    let json_data = JSON.parse(file_content);
+    for(let key in json_data){
+        switch(key){
+            case 'workshops':
+                for(let k=0; k < json_data[key].length; k++){
+                    let insert_data = {
+                        "workshop_title": json_data[key][k].workshop_title,
+                        "workshop_image": json_data[key][k].workshop_image
+                    }
+                    db.createNewWorkshop(insert_data);
+                }
+                break;
+
+            case 'categories':
+                for(let k=0; k < json_data[key].length; k++){
+                    let insert_data = {
+                        "category_title": json_data[key][k].category_title,
+                        "category_workshop": json_data[key][k].category_workshop
+                    }
+                    db.createNewCategory(insert_data);
+                }
+                break;
+            
+            case 'machines':
+                for(let k=0; k < json_data[key].length; k++){
+                    let insert_data = {
+                        "machine_title": json_data[key][k].machine_title,
+                        "machine_category": json_data[key][k].machine_category,
+                        "machine_brand": json_data[key][k].machine_brand,
+                        "machine_image": json_data[key][k].machine_image,
+                        "machine_reference": json_data[key][k].machine_reference
+                    }
+                    db.createNewMachine(insert_data);
+                }
+                break;
+            
+            case 'sections':
+                for(let k=0; k < json_data[key].length; k++){
+                    let insert_data = {
+                        "section_machine": json_data[key][k].section_machine,
+                        "section_type": json_data[key][k].section_type
+                    }
+                    db.createNewSection(insert_data);
+                }
+                break;
+
+            case 'parameters':
+                for(let k=0; k < json_data[key].length; k++){
+                    let insert_data = {
+                        "parameter_section": json_data[key][k].parameter_section,
+                        "parameter_name": json_data[key][k].parameter_name,
+                        "parameter_value": json_data[key][k].parameter_value
+                    }
+                    db.addParameter(insert_data);
+                }
+                break;
+        }
+    }
+    fs.unlinkSync(uploaded);
+    res.redirect('/parameters');
 });
 
 module.exports = router;
