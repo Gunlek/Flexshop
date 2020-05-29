49f6a9ae01b0f6483723cc618fd51d536839f7a1
Fabien Aubret <fabien.aubret@gmail.com>
2020-05-29 22:55:09 +0200

    [Fix] Fix import/export issues

--- src/node/web_interface/main.js
+++ src/node/web_interface/main.js
@@ -66,9 +66,9 @@ router.post('/import-json', upload.single('import_file'), async (req, res) => {
         switch(key){
             case 'workshops':
                 for(let k=0; k < json_data[key].length; k++){
-                    let insert_data = {
-                        "workshop_title": json_data[key][k].workshop_title,
-                        "workshop_image": json_data[key][k].workshop_image
+                    let insert_data = {};
+                    for(let subkey in json_data[key][k]){
+                        insert_data[subkey] = json_data[key][k][subkey];
                     }
                     db.createNewWorkshop(insert_data, ()=>{});
                 }
@@ -76,9 +76,9 @@ router.post('/import-json', upload.single('import_file'), async (req, res) => {
 
             case 'categories':
                 for(let k=0; k < json_data[key].length; k++){
-                    let insert_data = {
-                        "category_title": json_data[key][k].category_title,
-                        "category_workshop": json_data[key][k].category_workshop
+                    let insert_data = {};
+                    for(let subkey in json_data[key][k]){
+                        insert_data[subkey] = json_data[key][k][subkey];
                     }
                     db.createNewCategory(insert_data, ()=>{});
                 }
@@ -86,12 +86,9 @@ router.post('/import-json', upload.single('import_file'), async (req, res) => {
             
             case 'machines':
                 for(let k=0; k < json_data[key].length; k++){
-                    let insert_data = {
-                        "machine_title": json_data[key][k].machine_title,
-                        "machine_category": json_data[key][k].machine_category,
-                        "machine_brand": json_data[key][k].machine_brand,
-                        "machine_image": json_data[key][k].machine_image,
-                        "machine_reference": json_data[key][k].machine_reference
+                    let insert_data = {};
+                    for(let subkey in json_data[key][k]){
+                        insert_data[subkey] = json_data[key][k][subkey];
                     }
                     db.createNewMachine(insert_data, ()=>{});
                 }
@@ -99,9 +96,9 @@ router.post('/import-json', upload.single('import_file'), async (req, res) => {
             
             case 'sections':
                 for(let k=0; k < json_data[key].length; k++){
-                    let insert_data = {
-                        "section_machine": json_data[key][k].section_machine,
-                        "section_type": json_data[key][k].section_type
+                    let insert_data = {};
+                    for(let subkey in json_data[key][k]){
+                        insert_data[subkey] = json_data[key][k][subkey];
                     }
                     db.createNewSection(insert_data, ()=>{});
                 }
@@ -109,10 +106,9 @@ router.post('/import-json', upload.single('import_file'), async (req, res) => {
 
             case 'parameters':
                 for(let k=0; k < json_data[key].length; k++){
-                    let insert_data = {
-                        "parameter_section": json_data[key][k].parameter_section,
-                        "parameter_name": json_data[key][k].parameter_name,
-                        "parameter_value": json_data[key][k].parameter_value
+                    let insert_data = {};
+                    for(let subkey in json_data[key][k]){
+                        insert_data[subkey] = json_data[key][k][subkey];
                     }
                     db.addParameter(insert_data, ()=>{});
                 }
