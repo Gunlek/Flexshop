5b42b25752b314d9a9d20423673f0cbf6fbdbe60
Fabien Aubret <fabien.aubret@gmail.com>
2020-07-11 09:26:55 +0200

    [Add] Add database purge capabilities

--- src/node/web_interface/main.js
+++ src/node/web_interface/main.js
@@ -39,18 +39,21 @@ router.get('/export-json', (req, res) => {
             db.getAllMachines((machines) => {
                 db.getAllSections((sections) => {
                     db.getAllParameters((parameters) => {
-                        let json_export = {}
-                        json_export['workshops'] = workshops;
-                        json_export['categories'] = categories;
-                        json_export['machines'] = machines;
-                        json_export['sections'] = sections;
-                        json_export['parameters'] = parameters;
+                        db.getAllSlides((slides) => {
+                            let json_export = {}
+                            json_export['workshops'] = workshops;
+                            json_export['categories'] = categories;
+                            json_export['machines'] = machines;
+                            json_export['sections'] = sections;
+                            json_export['parameters'] = parameters;
+                            json_export['slides'] = slides;
 
-                        if(!fs.existsSync('./statics/exports')){
-                            fs.mkdirSync('./statics/exports');
-                        }
-                        fs.writeFileSync('./statics/exports/database_export.json', JSON.stringify(json_export));
-                        res.download('./statics/exports/database_export.json');
+                            if(!fs.existsSync('./statics/exports')){
+                                fs.mkdirSync('./statics/exports');
+                            }
+                            fs.writeFileSync('./statics/exports/database_export.json', JSON.stringify(json_export));
+                            res.download('./statics/exports/database_export.json');
+                        });
                     });
                 });
             });
@@ -113,10 +116,61 @@ router.post('/import-json', upload.single('import_file'), async (req, res) => {
                     db.addParameter(insert_data, ()=>{});
                 }
                 break;
+            
+            case 'slides':
+                for(let k=0; k < json_data[key].length; k++){
+                    let insert_data = {};
+                    for(let subkey in json_data[key][k]){
+                        insert_data[subkey] = json_data[key][k][subkey];
+                    }
+                    db.addSlide(insert_data, ()=>{});
+                }
+                break;
         }
     }
     fs.unlinkSync(uploaded);
     res.redirect('/');
+});
+
+router.get('/purge-database', (req, res) => {
+    // Delete all workshops
+    db.getAllWorkshops((ws_list) => {
+        ws_list.forEach((workshop) => {
+            db.deleteWorkshopById(workshop.workshop_id);
+        })
+    });
+    // Delete all machines
+    db.getAllMachines((m_list) => {
+        m_list.forEach((machine) => {
+            db.deleteMachineById(machine.machine_id);
+        })
+    });
+    // Delete all categories
+    db.getAllCategories((cat_list) => {
+        cat_list.forEach((category) => {
+            db.deleteCategoryById(category.category_id);
+        })
+    });
+    // Delete all parameters
+    db.getAllParameters((param_list) => {
+        param_list.forEach((parameter) => {
+            db.deleteParameterById(parameter.parameter_id);
+        })
+    });
+    // Delete all sections
+    db.getAllSections((sec_list) => {
+        sec_list.forEach((section) => {
+            db.deleteSectionById(section.section_id);
+        })
+    });
+    // Delete all slides
+    db.getAllSlides((slides_list) => {
+        slides_list.forEach((slide) => {
+            db.deleteSlideById(slide.slide_id);
+        })
+    });
+
+    res.redirect('/settings');
 });
 
 module.exports = router;
